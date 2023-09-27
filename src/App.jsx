14f92d8c02c93199642aee32197603028ee62913
Juan Mendoza <jsmendozap@unal.edu.co>
2023-09-26 19:48:12 -0500

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from './components/Sidebar';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        este es el sidebar
+    </div>
+  )
+}
+
+export default Sidebar
