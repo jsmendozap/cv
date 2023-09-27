863f3c4aaffecdae3b4426ce6c18cd1454915dcd
Juan Mendoza <jsmendozap@unal.edu.co>
2023-09-27 00:19:43 -0500

    Home listo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import forest from '../media/forest.jpg'
+import ReactTyped from "react-typed";
 
 const Home = () => {
   return (
@@ -8,7 +9,18 @@ const Home = () => {
         <div className="absolute top-24 left-1/2 flex items-center justify-center text-white">
             <div className="text-center">
                 <h1 className="text-5xl font-bold text-green-600">Juan Mendoza</h1>
-                <p className="mt-4 text-2xl text-red-500">Ingeniero Forestal</p>
+                <p className="mt-4 text-2xl text-gray-900 font-semibold">
+                    {''}
+                    <ReactTyped 
+                        strings={["Ingenieria Forestal",
+                                  "Sistemas de Información Geográfica",
+                                  "Modelación Forestal",
+                                  "Programación"
+                                  ]
+                                }
+                        typeSpeed={80} loop />    
+                </p>
+                
             </div>
         </div>
     </div>
