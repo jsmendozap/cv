9a771a763b636bf2b2e61af2f20347bb27d2c6f2
Juan Mendoza <jsmendozap@unal.edu.co>
2023-10-02 22:14:52 -0500

    interés rs completo

--- src/components/RS.jsx
+++ src/components/RS.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { useLang } from "../context/Lang";
+import fnf_en from "../media/fnf_en.gif";
+import fnf_es from "../media/fnf_es.gif";
 
 const RS = ({ info }) => {
   const { lang } = useLang();
 
   return (
-    <div className="flex">
-      <p className="text-justify text-lg m-6" style={{ fontFamily: "Mukta" }}>
+    <div>
+      <p
+        className="text-justify text-lg mt-6 mx-6"
+        style={{ fontFamily: "Mukta" }}
+      >
         {lang === "en" ? info.en.text[0] : info.es.text[0]}
       </p>
+      <img src={lang === "en" ? fnf_en : fnf_es} className="h-72" />
     </div>
   );
 };
