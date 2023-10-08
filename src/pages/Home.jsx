ace63a29ddf1698631898cf5ea2d6d5aa96c558c
Juan Mendoza <jsmendozap@unal.edu.co>
2023-10-07 23:45:50 -0500

    Cambio en el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,13 +9,17 @@ const Home = ({ info }) => {
 
   return (
     <div>
-      <img src={forest} alt="forest" className="lg:h-screen h-32 w-screen" />
+      <img
+        src={forest}
+        alt="forest"
+        className="lg:h-screen h-32 sm:h-60 w-screen"
+      />
       {screenWidth ? (
-        <div className="lg:absolute lg:top-24 lg:left-1/3 lg:ml-10 flex flex-col items-center justify-center">
-          <h1 className="lg:text-4xl text-xl font-bold text-green-600 max-md:mt-5">
+        <div className="absolute top-24 left-1/3 ml-10 flex flex-col items-center justify-center">
+          <h1 className="text-4xl text-xl font-bold text-green-600">
             Juan Sebastián Mendoza Páez
           </h1>
-          <p className="lg:mt-4 text-2xl text-gray-900 font-semibold">
+          <p className="mt-4 text-2xl text-gray-900 font-semibold">
             {""}
             <ReactTyped
               strings={lang === "es" ? info.es : info.en}
