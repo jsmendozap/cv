bcffb21508ec23fa196f0aa6d074825e96824cad
Juan Mendoza <jsmendozap@unal.edu.co>
2023-10-09 23:30:08 -0500

    instalación leaflet

--- src/components/RS.jsx
+++ src/components/RS.jsx
@@ -3,11 +3,22 @@ import { useLang } from "../context/Lang";
 import fnf_en from "../media/fnf_en.gif";
 import fnf_es from "../media/fnf_es.gif";
 import { Modal } from "antd";
+import { MapContainer, TileLayer } from "react-leaflet";
+import GeoTiff from "./GeoTiff";
 
 const RS = ({ info }) => {
   const { lang } = useLang();
   const [isOpen, setOpen] = useState(false);
 
+  /*
+<img
+  src={lang === "en" ? fnf_en : fnf_es}
+  className="h-72 cursor-pointer"
+  onClick={() => {
+    setOpen(true);
+  }}
+/>
+*/
   const handleOk = () => {
     setOpen(false);
   };
@@ -20,13 +31,14 @@ const RS = ({ info }) => {
       >
         {lang === "en" ? info.en.text[0] : info.es.text[0]}
       </p>
-      <img
-        src={lang === "en" ? fnf_en : fnf_es}
-        className="h-72 cursor-pointer"
-        onClick={() => {
-          setOpen(true);
-        }}
-      />
+      <div id="map" className="w-96 mt-5 ml-6">
+        <MapContainer center={[4, -75]} className="h-52 rounded-md" zoom={3}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          />
+        </MapContainer>
+      </div>
       <Modal
         width={750}
         open={isOpen}
