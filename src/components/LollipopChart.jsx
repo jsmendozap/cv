360c7862c66baf31af3922b989eceda7d85ca2f7
Juan Mendoza <jsmendozap@unal.edu.co>
2023-10-13 21:42:30 -0500

    Lollipop chart listo

---
+++ src/components/LollipopChart.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import fnf from "../assets/fnf.json";
+import Plot from "react-plotly.js";
+import { useLang } from "../context/Lang";
+import fnf_2017 from "../media/2017.png";
+import fnf_2018 from "../media/2018.png";
+import fnf_2019 from "../media/2019.png";
+import fnf_2020 from "../media/2020.png";
+
+const LollipopChart = ({ year }) => {
+  const { lang } = useLang();
+  const [img, setImg] = useState(null);
+
+  useEffect(() => {
+    // Cargar la imagen según el año
+    let imgSrc;
+    switch (year) {
+      case 2017:
+        imgSrc = fnf_2017;
+        break;
+      case 2018:
+        imgSrc = fnf_2018;
+        break;
+      case 2019:
+        imgSrc = fnf_2019;
+        break;
+      case 2020:
+        imgSrc = fnf_2020;
+        break;
+    }
+    setImg(imgSrc);
+  }, [year]);
+
+  const data = {};
+
+  fnf.forEach((item) => {
+    const { time, lc, n, area, lc_es } = item;
+
+    if (!data[time]) {
+      data[time] = [];
+    }
+
+    data[time].push({
+      x: area,
+      y: lang === "es" ? lc_es : lc,
+    });
+  });
+
+  const filterData = [year].map((item) => [item, data[item]])[0][1];
+
+  const plot = [
+    {
+      x: filterData.map((dato) => dato.x),
+      y: filterData.map((dato) => dato.y),
+      type: "scatter",
+      mode: "markers",
+      marker: { color: "red" },
+    },
+  ];
+
+  const shapes = filterData.map((dato) => ({
+    type: "line",
+    xref: "x",
+    yref: "y",
+    x0: 0,
+    x1: dato.x,
+    y0: dato.y,
+    y1: dato.y,
+    line: {
+      color: "grey",
+      width: 1,
+    },
+  }));
+
+  const layout = {
+    width: 360,
+    height: 300,
+    xaxis: {
+      title: lang === "es" ? "Área (Km<sup>2</sup>)" : "Area (Km<sup>2</sup>)",
+    },
+    title: {
+      text:
+        lang === "es"
+          ? `Mapa bosque - no bosque <br><sup>La Macarena, Meta - año: ${year}</sup>`
+          : `Forest/no Forest Map <br><sup>La Macarena, Meta - year: ${year}</sup>`,
+      font: { family: "Bree Serif", size: 20 },
+      x: 0.3,
+      y: 0.9,
+    },
+    shapes,
+    margin: { l: 120, t: 70, b: 100, r: 30 },
+  };
+
+  return (
+    <div className="flex flex-row">
+      <Plot data={plot} layout={layout} config={{ displayModeBar: false }} />
+      <img src={img} alt="img" className="w-72 h-64 pt-3 mr-5" />
+    </div>
+  );
+};
+
+export default LollipopChart;
--- src/components/RS.jsx
+++ src/components/RS.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLang } from "../context/Lang";
-import { MapContainer, TileLayer, ImageOverlay, Marker } from "react-leaflet";
-import { LatLngBounds } from "leaflet";
+import LollipopChart from "./LollipopChart";
 
 const RS = ({ info }) => {
   const { lang } = useLang();
+  const [year, setYear] = useState(2017);
 
-  const url =
-    "https://github.com/jsmendozap/cv/blob/leaflet/src/media/2017.png";
-  /*const bounds = new LatLngBounds(
-    [2.64, -73.4415555555556],
-    [1.60422222222222, -74.7488888888889]
-  );*/
-  const bounds = new LatLngBounds(
-    [40.712216, -74.22655],
-    [40.773941, -74.12544]
-  );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setYear((prevYear) => (prevYear === 2020 ? 2017 : prevYear + 1));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div>
@@ -25,16 +24,7 @@ const RS = ({ info }) => {
       >
         {lang === "en" ? info.en.text[0] : info.es.text[0]}
       </p>
-      <div id="map" className="w-96 mt-5 ml-6">
-        <MapContainer center={[4, -75]} className="h-52 rounded-md" zoom={3}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          />
-          <ImageOverlay url={url} bounds={bounds} alt="prueba" opacity={0.4} />
-          <Marker position={[40.712216, -74.22655]} />
-        </MapContainer>
-      </div>
+      <LollipopChart year={year} />
     </div>
   );
 };
